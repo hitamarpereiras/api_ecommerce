ce8f8d6bb90934d0de61d7c3de9131c5cca4cc88
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2026-02-17 10:05:13 -0300

    Atualização do app customers

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customers(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.PROTECT,
+        related_name='customers',
+        verbose_name='Usuário'
+    )
+    name = models.CharField(max_length=100, verbose_name='Nome')
+    phone = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True,
+        verbose_name='Telefone'
+    )
+    address = models.TextField(
+        max_length=200,
+        blank=True,
+        null=True,
+        verbose_name='Endereço'
+    )
+    number = models.IntegerField(
+        max_length=10,
+        blank=True,
+        null=True,
+        verbose_name='Número da casa'
+    )
+    avatar_url = models.URLField(
+        blank=True,
+        null=True,
+        verbose_name='Avatar'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Criado em',
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Atualizado em',
+    )
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+
+    def __str__(self):
+        return f"{self.name}"

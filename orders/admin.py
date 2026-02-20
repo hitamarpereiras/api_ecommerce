cc2290e1fd94b75d34acca14eec96ca908ed8105
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2026-02-20 09:53:16 -0300

    App Order registrado

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from orders.models import Order
 
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'customer',
+        'name_customer',
+        'phone',
+        'address',
+        'total',
+        'created_at'
+    ]
+    search_fields = [
+        'phone',
+        'created_at',
+    ]

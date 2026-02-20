2101f6d8225c36a01318a0847d71d43128ad83aa
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2026-02-20 10:15:23 -0300

    App Sales criado

---
+++ sales/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from accounts.models import Account
+from orders.models import Order
+
+
+class Sale(models.Model):
+    total = models.DecimalField(
+        default=0,
+        decimal_places=2,
+        max_digits=10,
+        verbose_name='Total'
+    )
+    subtotal = models.DecimalField(
+        default=0,
+        decimal_places=2,
+        max_digits=10,
+        verbose_name='Subtotal'
+    )
+    remaining = models.DecimalField(
+        default=0,
+        decimal_places=2,
+        max_digits=10,
+        verbose_name='Troco'
+    )
+    payment_method = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True,
+        verbose_name='Pago com'
+    )
+    rate_delivery = models.DecimalField(
+        default=0,
+        decimal_places=2,
+        max_digits=10,
+        verbose_name='Taxa de Entrega'
+    )
+    account = models.ForeignKey(
+        Account,
+        on_delete=models.PROTECT,
+        verbose_name='Conta'
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.PROTECT,
+        verbose_name='Pedido'
+    )
+    status = models.BooleanField(default=False)
+    collaborator = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Colaborador'
+    )
+    observation = models.TextField(
+        max_length=200,
+        blank=True,
+        null=True,
+        verbose_name='Observação'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Atualizado em',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Criado em',
+    )
+
+
+    class Meta:
+        verbose_name = 'Venda'
+        verbose_name_plural = 'Vendas'
+
+    def __str__(self):
+        return f"{self.created_at}"

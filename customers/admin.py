1f075f168b581c8514a3fce48c6527a5227ae323
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2026-02-25 19:29:00 -0300

    Atualização no app customers/admin

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from customers.models import Customer
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'phone',
+        'address',
+        'house_number',
+        'coins',
+        'avatar_url',
+        'created_at',
+        'updated_at',
+    ]
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['name', 'phone', 'address']
